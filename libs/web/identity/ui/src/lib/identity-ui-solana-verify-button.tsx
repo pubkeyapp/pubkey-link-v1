7c8deea63e3a3d6d7cf85a842824d389c2b05630
Bram Borggreve <beeman@beeman.nl>
2024-01-21 18:35:31 +0000

    fix: only show Identity Verify button on Solana identities

--- libs/web/identity/ui/src/lib/identity-ui-solana-verify-button.tsx
+++ libs/web/identity/ui/src/lib/identity-ui-solana-verify-button.tsx
@@ -1,13 +1,13 @@
 import { Button, Tooltip } from '@mantine/core'
 import { modals } from '@mantine/modals'
-import { ellipsify, Identity } from '@pubkey-link/sdk'
+import { ellipsify, Identity, IdentityProvider } from '@pubkey-link/sdk'
 import { IdentityProviderSolana } from '@pubkey-link/web/identity/data-access'
 import { SolanaClusterProvider } from '@pubkey-link/web/solana/data-access'
 import { IconAlertCircle } from '@tabler/icons-react'
 import { IdentityUiSolanaVerifyWizard } from './identity-ui-solana-verify-wizard'
 
 export function IdentityUiSolanaVerifyButton({ identity, refresh }: { identity: Identity; refresh: () => void }) {
-  return (
+  return identity.provider === IdentityProvider.Solana ? (
     <Tooltip label={`Verify ${ellipsify(identity.providerId)} by signing a message with your wallet.`}>
       <Button
         size="xs"
@@ -30,5 +30,5 @@ export function IdentityUiSolanaVerifyButton({ identity, refresh }: { identity: 
         Verify Identity
       </Button>
     </Tooltip>
-  )
+  ) : null
 }
